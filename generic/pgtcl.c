2ef9306fc687b190b8ce883021706d6e4ca42a7d
Peter da Silva <peter.dasilva@flightaware.com>
2017-05-26 12:24:47 +0000

    Make pg_sqlite depend on sqlite3 library being present, with option to disable in configure --with-sqlite3=no

--- generic/pgtcl.c
+++ generic/pgtcl.c
@@ -95,7 +95,9 @@ static PgCmd commands[] = {
     {"pg_dbinfo", "::pg::dbinfo", Pg_dbinfo,2},
     {"pg_getdata", "::pg::getdata", Pg_getdata,2},
     {"pg_sql", "::pg::sql", Pg_sql,2},
+#ifdef HAVE_SQLITE3
     {"pg_sqlite", "::pg::sqlite", Pg_sqlite, 3},
+#endif
     {NULL, NULL, NULL, 0}
 };
 
