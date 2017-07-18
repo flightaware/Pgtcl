81d657f7b2fca5988a74f7bb28118b47e2e3de97
Peter da Silva <peter.dasilva@flightaware.com>
2017-07-18 19:52:24 +0000

    Cleanup tokenizer for integration with Pgtcl.
    * Remove testcase calls
    * Remove EBCDIC
    * Define TK_* return types as enum in tokenize.h
    * Split TK_VARIABLE into TK_SQLVAR and TK_TCLVAR
    * Update configure.in

---
+++ generic/tokenize.h
@@ -0,0 +1,13 @@
+/*
+** tokenize.h
+**
+** An tokenizer for SQL
+*/
+
+enum sqltoken {
+TK_BITAND, TK_BITNOT, TK_BITOR, TK_BLOB, TK_COMMA, TK_CONCAT, TK_DOT, TK_EQ, TK_FLOAT, TK_GE,
+TK_GT, TK_ID, TK_ILLEGAL, TK_INTEGER, TK_LE, TK_LP, TK_LSHIFT, TK_LT, TK_MINUS, TK_NE, TK_PLUS,
+TK_REM, TK_RP, TK_RSHIFT, TK_SEMI, TK_SLASH, TK_SPACE, TK_STAR, TK_STRING, TK_TCLVAR, TK_SQLVAR
+};
+
+int Pg_sqlite3GetToken(const unsigned char *z, enum sqltoken *tokenType){
